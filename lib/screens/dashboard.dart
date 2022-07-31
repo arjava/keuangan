2b62aabdb8a021c85417b87a02b56d451a0b59d9
arjava <arjava379@gmail.com>
2022-07-31 23:20:03 +0700

    clean code dashboard and constant and add debug only for print

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,9 +1,7 @@
 // import 'dart:ffi';
 
 import 'package:cron/cron.dart';
-import 'package:floating_navbar/floating_navbar.dart';
-import 'package:floating_navbar/floating_navbar_item.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:keuangan/model/response_background_hit.dart';
 import 'package:keuangan/provider/cron_provider.dart';
@@ -12,7 +10,6 @@ import 'package:keuangan/screens/report_page.dart';
 import 'package:keuangan/screens/tools_page.dart';
 import 'package:keuangan/screens/transaction_page.dart';
 import 'package:keuangan/utils/Constant.dart';
-import 'package:shape_of_view_null_safe/shape_of_view_null_safe.dart';
 
 class DashboardScreen extends StatefulWidget {
   static const routeName = '/dashboard';
@@ -37,12 +34,16 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
     try {
       cron.schedule(Schedule.parse('0/30 * * * * ?'), () async {
-        print(DateTime.now());
+        if (kDebugMode) {
+          print(DateTime.now());
+        }
         CronApiProvider _apiProvider = CronApiProvider();
         Object response = await _apiProvider.hitBackground();
         HitBackgroundResponse responseBackground =
             response as HitBackgroundResponse;
-        print(responseBackground.data?.sender?.email);
+        if (kDebugMode) {
+          print(responseBackground.data?.sender?.email);
+        }
       });
 
       // await Future.delayed(Duration(seconds: 30));
@@ -67,10 +68,10 @@ class _DashboardScreenState extends State<DashboardScreen> {
         floatingActionButtonLocation: FloatingActionButtonLocation.centerTop,
         appBar: AppBar(
           backgroundColor: Color(0xffffffff),
-          title: Container(
+          title: SizedBox(
             height: MediaQuery.of(context).size.height / 5,
             width: MediaQuery.of(context).size.width,
-            child: Center(
+            child: const Center(
                 child: Text(
               Constants.appName,
               style: TextStyle(color: Color(0xff2787BD)),
@@ -82,79 +83,71 @@ class _DashboardScreenState extends State<DashboardScreen> {
                 icon: Image.asset("assets/btn_notifikasi.png"))
           ],
           bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(75.0),
               child: Container(
-                margin: EdgeInsets.only(bottom: 20),
+                margin: const EdgeInsets.only(bottom: 20),
                 child: BottomNavigationBar(
                     currentIndex: indexTab,
-                    onTap: (_currentIndex) {
+                    onTap: (currentIndex) {
                       if (indexTab == 0) {
                         setState(() {
-                          indexTab = _currentIndex;
-                          SnackBar(content: Text("Home Page"));
+                          indexTab = currentIndex;
                           _pageController.jumpToPage(indexTab);
-                          // Navigator.pushNamed(context, '/home');
-                          // Navigator.push(
-                          // context,
-                          // MaterialPageRoute(
-                          // builder: (context) => HomePage()));
                         });
                       } else if (indexTab == 1) {
                         setState(() {
-                          indexTab = _currentIndex;
+                          indexTab = currentIndex;
                           _pageController.jumpToPage(indexTab);
                         });
-                        SnackBar(content: Text("Transaksi Page"));
                       } else if (indexTab == 2) {
                         setState(() {
-                          indexTab = _currentIndex;
+                          indexTab = currentIndex;
                           _pageController.jumpToPage(indexTab);
                         });
-                        SnackBar(content: Text("Report Page"));
                       } else if (indexTab == 3) {
                         setState(() {
-                          indexTab = _currentIndex;
+                          indexTab = currentIndex;
                           _pageController.jumpToPage(indexTab);
                         });
-                        SnackBar(content: Text("Tools Page"));
                       }
                     },
                     showUnselectedLabels: true,
-                    unselectedItemColor: Color.fromARGB(129, 39, 134, 189),
-                    selectedItemColor: Color(0xff2787BD),
+                    unselectedItemColor:
+                        const Color.fromARGB(129, 39, 134, 189),
+                    selectedItemColor: const Color(0xff2787BD),
                     elevation: 0,
-                    backgroundColor: Color(0xffffffff),
+                    backgroundColor: const Color(0xffffffff),
                     items: [
                       BottomNavigationBarItem(
                           activeIcon: Image.asset("assets/btn_home_aktif.png"),
-                          backgroundColor: Color(0xffffffff),
+                          backgroundColor: const Color(0xffffffff),
                           label: "Home",
                           icon: Image.asset("assets/btn_home_non_active.png")),
                       BottomNavigationBarItem(
                           activeIcon:
                               Image.asset("assets/btn_transaksi_aktif.png"),
-                          backgroundColor: Color(0xffffffff),
+                          backgroundColor: const Color(0xffffffff),
                           label: "Transaksi",
                           icon: Image.asset(
                               "assets/btn_transaction_non_active.png")),
                       BottomNavigationBarItem(
                           activeIcon:
                               Image.asset("assets/btn_laporan_aktif.png"),
-                          backgroundColor: Color(0xffffffff),
+                          backgroundColor: const Color(0xffffffff),
                           label: "Laporan",
                           icon:
                               Image.asset("assets/btn_report_non_active.png")),
                       BottomNavigationBarItem(
                           activeIcon: Image.asset("assets/btn_tools_aktif.png"),
-                          backgroundColor: Color(0xffffffff),
+                          backgroundColor: const Color(0xffffffff),
                           label: "Tools",
                           icon: Image.asset("assets/btn_tools_non_active.png"))
                     ]),
-              ),
-              preferredSize: Size.fromHeight(75.0)),
+              )),
         ),
         body: PageView(
           controller: _pageController,
-          children: <Widget>[
+          children: const <Widget>[
             HomePage(),
             TransactionPage(),
             ReportPage(),
@@ -165,40 +158,6 @@ class _DashboardScreenState extends State<DashboardScreen> {
               indexTab = page;
             });
           },
-        )
-        // Material(
-        //   child: FloatingNavBar(
-        //       index: indexTab,
-        //       resizeToAvoidBottomInset: true,
-        //       horizontalPadding: 20,
-        //       items: [
-        //         FloatingNavBarItem(
-        //             icon: ImageIcon(AssetImage("assets/btn_home_aktif.png")),
-        //             useImageIcon: true,
-        //             title: "Home",
-        //             page: HomePage()),
-        //         FloatingNavBarItem(
-        //             icon: ImageIcon(
-        //                 AssetImage("assets/btn_transaction_non_active.png")),
-        //             useImageIcon: true,
-        //             title: "Transaksi",
-        //             page: TransactionPage()),
-        //         FloatingNavBarItem(
-        //             icon:
-        //                 ImageIcon(AssetImage("assets/btn_report_non_active.png")),
-        //             useImageIcon: true,
-        //             title: "Laporan",
-        //             page: ReportPage()),
-        //         FloatingNavBarItem(
-        //             icon:
-        //                 ImageIcon(AssetImage("assets/btn_tools_non_active.png")),
-        //             useImageIcon: true,
-        //             title: "Tools",
-        //             page: ToolsPage())
-        //       ],
-        //       color: Color(0xffffffff),
-        //       hapticFeedback: true),
-        // ),
-        );
+        ));
   }
 }
--- lib/utils/Constant.dart
+++ lib/utils/Constant.dart
@@ -1,4 +1,13 @@
-import 'package:dio/dio.dart';
+// ignore: file_names
+import 'package:dio/dio.dart'
+    show
+        Dio,
+        DioError,
+        DioErrorType,
+        InterceptorsWrapper,
+        RequestOptions,
+        Response;
+import 'package:flutter/foundation.dart';
 
 class Constants {
   static String endpoint(String url) {
@@ -14,16 +23,20 @@ class Constants {
   static const String logoTag = 'com.arjavax.logo';
   static const String titleTag = 'com.arjavax.title';
 
-  static void setupLoggingInterceptor(Dio? _dio) {
+  static void setupLoggingInterceptor(Dio? dio) {
     int? maxCharactersPerLine = 200;
 
-    _dio?.interceptors.add(
+    dio?.interceptors.add(
         InterceptorsWrapper(onRequest: (RequestOptions options, handler) async {
-      print(options.data);
+      if (kDebugMode) {
+        print(options.data);
+      }
       return handler.next(options);
     }, onResponse: (Response? response, handler) async {
-      print(
-          "<-- ${response?.statusCode} ${response?.requestOptions.method} ${response?.requestOptions.path}");
+      if (kDebugMode) {
+        print(
+            "<-- ${response?.statusCode} ${response?.requestOptions.method} ${response?.requestOptions.path}");
+      }
       String? responseAsString = response?.data.toString();
       if (responseAsString!.length > maxCharactersPerLine) {
         int? iterations =
@@ -33,42 +46,35 @@ class Constants {
           if (endingIndex > responseAsString.length) {
             endingIndex = responseAsString.length;
           }
-          print(responseAsString.substring(
-              i * maxCharactersPerLine, endingIndex));
+          if (kDebugMode) {
+            print(responseAsString.substring(
+                i * maxCharactersPerLine, endingIndex));
+          }
         }
       } else {
-        print(response?.data);
+        if (kDebugMode) {
+          print(response?.data);
+        }
       }
-      print("<-- END HTTP");
+      if (kDebugMode) {
+        print("<-- END HTTP");
+      }
       return handler.next(response!);
     }, onError: (DioError e, handler) async {
-      String errorDescription = "";
-      if (e is DioError) {
-        switch (e.type) {
-          case DioErrorType.cancel:
-            errorDescription = "Request to API server was cancelled";
-            break;
-          case DioErrorType.connectTimeout:
-            errorDescription = "Connection timeout with API server";
-            break;
-          case DioErrorType.other:
-            errorDescription =
-                "Connection to API server failed due to internet connection";
-            break;
-          case DioErrorType.receiveTimeout:
-            errorDescription = "Receive timeout in connection with API server";
-            break;
-          case DioErrorType.response:
-            errorDescription =
-                "Received invalid status code: ${e.response?.statusCode}";
-            break;
-          case DioErrorType.sendTimeout:
-            break;
-        }
-      } else {
-        errorDescription = "Unexpected error occured";
+      switch (e.type) {
+        case DioErrorType.cancel:
+          break;
+        case DioErrorType.connectTimeout:
+          break;
+        case DioErrorType.other:
+          break;
+        case DioErrorType.receiveTimeout:
+          break;
+        case DioErrorType.response:
+          break;
+        case DioErrorType.sendTimeout:
+          break;
       }
-
       return handler.next(e);
     }));
   }
